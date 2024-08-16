f0640383f873dc08ea1e577aff9e9f6022225f87
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2024-08-16 14:26:17 +0530

    comment added

--- example-app/database/migrations/2024_08_15_110550_create_employees_table.php
+++ example-app/database/migrations/2024_08_15_110550_create_employees_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('employees', function (Blueprint $table) {
+            $table->comment('Employee Details');
             $table->id();
             $table->string('first_name', length: 50);
             $table->string('last_name', length: 50);
