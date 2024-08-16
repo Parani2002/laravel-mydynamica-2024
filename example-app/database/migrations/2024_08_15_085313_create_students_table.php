d6c92de98214a1923142be957d4d6a5b28b97ea8
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2024-08-16 12:10:40 +0530

    add comment to table

--- example-app/database/migrations/2024_08_15_085313_create_students_table.php
+++ example-app/database/migrations/2024_08_15_085313_create_students_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('students', function (Blueprint $table) {
+            $table->comment('Students Details');
             $table->id();
             $table->string('first_name', length: 50);
             $table->string('last_name', length: 50);
