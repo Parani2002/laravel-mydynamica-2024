c1ac75ece020a7485a4d78e0ca28b1976c0af44d
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2024-08-24 16:53:12 +0530

    bootstrap installed

--- example-app/resources/views/components/layout.blade.php
+++ example-app/resources/views/components/layout.blade.php
@@ -1,17 +1,19 @@
 <html lang="en">
-<x-head />
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js','public/assets/css/styles.css','public/assets/js/datatables-simple-demo.js','public/assets/js/scripts.js','public/assets/demo/chart-area-demo.js',
+                'public/assets/demo/chart-bar-demo.js',
+                'public/assets/demo/chart-pie-demo.js',
+                'public/assets/demo/datatables-demo.js']);
+</head>
 <body>
     <x-topnav />
-   
-    
     <x-sidenav />
     {{$slot}}
     <x-footer />
-    
-       
-    
-       
-    
 <x-scripts />  
 </body>
 </html>
--- example-app/vite.config.js
+++ example-app/vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "public/assets/css/styles.css",
+                "public/assets/js/datatables-simple-demo.js",
+                "public/assets/js/scripts.js",
+                'public/assets/demo/chart-area-demo.js',
+                'public/assets/demo/chart-bar-demo.js',
+                'public/assets/demo/chart-pie-demo.js',
+                'public/assets/demo/datatables-demo.js'
+            ],
             refresh: true,
         }),
     ],
