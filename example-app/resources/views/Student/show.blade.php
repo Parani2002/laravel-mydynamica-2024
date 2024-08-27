9fe0e793cf70ed8f8571ccf826a2560e7b1cec11
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2024-08-27 16:36:20 +0530

    show blade modified

--- example-app/resources/views/Student/show.blade.php
+++ example-app/resources/views/Student/show.blade.php
@@ -58,58 +58,93 @@
                     </div>
                 </div>
             </div>
-        <div class="container-fluid px-4">
-            <h1 class="mt-4">Tables</h1>
-            <ol class="breadcrumb mb-4">
-                {{-- <li class="breadcrumb-item"><a href="/">Dashboard</a></li>
-                <li class="breadcrumb-item"><a href="/grades">grades</a></li>
-                <li class="breadcrumb-item">{{$id}}</li> --}}
-
-            </ol>
-            <div class="card mb-4">
-                <div class="card-body">
-                    DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the
-                    <a target="_blank" href="https://datatables.net/">official DataTables documentation</a>
-                    
+            <div class="container-fluid px-4">
+                <h1 class="mt-4">Grade</h1>
+                <ol class="breadcrumb mb-4">
+                    <li class="breadcrumb-item"><a href="/">Dashboard</a></li>
+                    <li class="breadcrumb-item active">Grade Tables</li>
+                </ol>
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="mb-0">{{$student -> grade-> grade_name}} Grade Details</h3>
+                    </div>
+                    <div class="card-body">
+                        <table id="datatablesSimple" class="table table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Attribute</th>
+                                    <th>Value</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Student ID</td>
+                                    <td>{{ $student-> id }}</td>
+                                </tr>
+                                <tr>
+                                    <td>First Name</td>
+                                    <td>{{ $student-> first_name }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Last Name</td>
+                                    <td>{{ $student -> last_name }}</td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
-
-            <div class="row">
-                <div class="col-xl-6">
-                    <div class="card mb-4">
-                        <div class="card-header">
-                        <i class="fas fa-chart-area me-1"></i>
-                            Student Details
-                        </div>
-                    {{-- <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div> --}}
-                        <div class="card-body">
-                            <table id="datatablesSimple">
+                <br>
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="mb-0">Subject Details</h3>
+                    </div>
+                    <div class="card-body">
+                        <table id="datatablesSimple" class="table table-striped table-bordered">
+                            <tbody>
+                                <tr>
+                                    <th>Subject ID</th>
+                                    <th>Subject Name</th>
+                                    <th>Subject Order</th>
+                                    <th>Subject Color</th>
+                                </tr>
+                                @foreach ($student->subjects as $subject)
+                                    <tr>
+                                        <td>{{ $subject->id }}</td>
+                                        <td>{{ $subject->subject_name }}</td>
+                                        <td>{{ $subject->subject_order }}</td>
+                                        <td>{{ $subject->subject_color }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="mb-0">Student Details</h3>
+                    </div>
+                    <div class="card-body">
+                        <table id="datatablesSimple" class="table table-striped table-bordered">
+                            <tbody>
                                 <tr>
                                     <th>Student ID</th>
-                                    <td>{{$student -> id}}</td>
-                                </tr>
-                                <tr>
                                     <th>First Name</th>
-                                    <td>{{$student -> first_name}}</td>
-                                </tr>
-                                <tr>
                                     <th>Last Name</th>
-                                    <td>{{$student -> last_name}}</td>
                                 </tr>
-                                <tr>
-                                    <th>Grade Name</th>
-                                    <td>{{$student -> grade -> grade_name}}</td>
-                                </tr>
-                            </table>
-                        </div>
+                                @foreach ($students as $student)
+                                    <tr>
+                                        <td>{{ $student->id }}</td>
+                                        <td> {{ $student->first_name }}</td>
+                                        <td>{{ $student->last_name }}</td>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
-           
-           
-           
-       
-        </div>
 
 </main>
 </x-layout>
