f91a8d91dab6a7102cf111dee8d250394e6a8af7
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2024-08-28 09:32:10 +0530

    icons added
    student index page

--- example-app/resources/views/Student/index.blade.php
+++ example-app/resources/views/Student/index.blade.php
@@ -11,9 +11,10 @@
             <div class="row">
                 <div class="col-xl-3 col-md-6">
                     <div class="card bg-primary text-white mb-4">
-                        <div style="display: flex">
-                            <div class="card-body">Students</div>
-                            <div class="card-body">{{$students_count}}</div>
+                        <div style="display: flex; justify-content: center;padding:5px">
+                            <i class="fas fa-user-graduate" style="align-self: center; width:30px;height:50px;padding-left:10px"></i>
+                            <div class="card-body"><h3>Students</h3></div>
+                            <div class="card-body"><h3>{{$students_count}}</h3></div>
                         </div>
                         
 
@@ -25,9 +26,10 @@
                 </div>
                 <div class="col-xl-3 col-md-6">
                     <div class="card bg-warning text-white mb-4">
-                        <div style="display: flex">
-                            <div class="card-body">Grades</div>
-                            <div class="card-body">{{$grades_count}}</div>
+                        <div style="display: flex;justify-content: center;padding:5px;">
+                            <i class="fas fa-clipboard-list" style="align-self: center; width:30px;height:50px;padding-left:10px"></i>
+                            <div class="card-body"><h3>Grades</h3></div>
+                            <div class="card-body"><h3>{{$grades_count}}</h3></div>
                         </div>
                         <div class="card-footer d-flex align-items-center justify-content-between">
                             <a class="small text-white stretched-link" href="/grades">View Details</a>
@@ -37,9 +39,10 @@
                 </div>
                 <div class="col-xl-3 col-md-6">
                     <div class="card bg-success text-white mb-4">
-                        <div style="display: flex;">
-                            <div class="card-body">Subjects</div>
-                            <div class="card-body">{{$subjects_count}}</div>
+                        <div style="display: flex;justify-content: center;padding:5px;">
+                            <i class="fas fa-book-open" style="align-self: center; width:30px;height:50px;padding-left:10px"></i>
+                            <div class="card-body"><h3>Subjects</h3></div>
+                            <div class="card-body"><h3>{{$subjects_count}}</h3></div>
                         </div>
                         <div class="card-footer d-flex align-items-center justify-content-between">
                             <a class="small text-white stretched-link" href="/subjects">View Details</a>
